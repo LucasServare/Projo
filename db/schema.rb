7fb747dffa3d2c9eefb5bcc319df321652292a7f
Lucas <lucas.servare@gmail.com>
2015-11-28 10:47:29 -0600

    Add a User model, give them a secure password with Bcrypt, create a migration for users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151128164034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name", limit: 30
+    t.string   "string",     limit: 30
+    t.string   "last_name",  limit: 30
+    t.string   "email",                 default: "", null: false
+    t.string   "password",   limit: 40
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+  end
 
 end
