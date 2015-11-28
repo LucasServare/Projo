0a1c13f33d168735d65796821a5e432d3a1ccb96
Lucas <lucas.servare@gmail.com>
2015-11-28 10:35:24 -0600

    Flesh out home page, add necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get "public" => "public#home"
+  get "public/home" => "public#home"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
