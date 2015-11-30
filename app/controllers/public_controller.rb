01716847d73fc35afea6d565ca384d067984bd6f
Lucas <lucas.servare@gmail.com>
2015-11-29 18:37:31 -0600

    Move some of the user functions into a new users controller to clean up code a bit.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,26 +3,4 @@ class PublicController < ApplicationController
   def home
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(subject_params)
-    if @user.save
-      redirect_to(:action => 'user_settings')
-    else
-      render('new')
-    end
-  end
-
-  def user_settings
-  end
-
-  private
-
-    def subject_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-    end
-
 end
