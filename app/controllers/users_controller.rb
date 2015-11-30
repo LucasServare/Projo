90d4f097e342489a9bd50518f5ef7fa47b744999
Lucas <lucas.servare@gmail.com>
2015-11-30 15:13:33 -0600

    Follow convention more closely with the edit action. Allow users to delete their accounts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,25 +7,34 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to(:action => 'edit_user_settings', id: @user.id)
+      redirect_to(:action => 'edit', id: @user.id)
     else
       render('new')
     end
   end
 
-  def edit_user_settings
+  def edit
     @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      redirect_to(:action => 'edit_user_settings', id: @user.id)
+      redirect_to(:action => 'edit', id: @user.id)
     else
       render('new')
     end
   end
 
+  def delete
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to(:action => 'home', :controller => 'public')
+  end
+  
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
