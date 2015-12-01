006cd6ddea0f893793ce85bef66578f14d61e7bf
Lucas <lucas.servare@gmail.com>
2015-12-01 14:40:45 -0600

    Add a join table for users and projects.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :projects
+  has_and_belongs_to_many :projects
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130012103) do
+ActiveRecord::Schema.define(version: 20151201203443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name",       limit: 30
+    t.integer  "creator_id"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
+
+  create_table "projects_users", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "project_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",      limit: 30
