1be8f9b142717661aae81cab6055617b2e158c1a
Lucas <lucas.servare@gmail.com>
2016-01-06 18:33:29 -0600

    Remove unnecessary migration.

--- db/migrate/20151215015400_add_to_do_list.rb
+++ db/migrate/20151215015400_add_to_do_list.rb
@@ -3,7 +3,7 @@ class AddToDoList < ActiveRecord::Migration
     create_table(:to_do) do |t|
       t.string :content, limit:100
       t.belongs_to :user, index:true
-      t.boolean :completed
+      t.boolean :completed, default => false
       t.timestamps null: false
     end
   end
