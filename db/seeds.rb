2315caa90b5dc182e7c1d224e5b9a3efcc74a742
Lucas <lucas.servare@gmail.com>
2016-01-08 20:37:12 -0600

    Add some more seeds, and comment them to explain a bit better.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
+#Create an initial user
 user = User.create(first_name: "Lucas", last_name: "Servare", email: "lucas.servare@gmail.com", password: "test")
+#Add another one
+other_user = User.create(first_name: "Other", last_name: "Person", email: "other_email@fakeemail.com", password: "test")
+#Create an initial project for our first user
 project = user.projects.create(name: "Test Project", creator_id: user.id)
+#Add another user to this project
+project.users  << other_user
+#Create a to do item for the initial user.
+to_do_items = user.to_do_items.create(content: "Test Item")
